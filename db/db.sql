8cb282839ef7e11a073d9e5e407b76da97450ab5
Nicolas Julian Torres <71136123+JulianTorres1@users.noreply.github.com>
2025-02-26 10:08:52 -0500

    test fotos

--- db/db.sql
+++ db/db.sql
@@ -12,9 +12,45 @@ CREATE TABLE votaciones (
     id_voto INT AUTO_INCREMENT PRIMARY KEY,
     id_candidato INT,
     -- ID del candidato que recibió el voto
-    id_estudiante INT,
     -- ID del estudiante que votó
     fecha_voto TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_candidato) REFERENCES candidatos(id_candidato) ON DELETE CASCADE
-);-- Tabla de Resultados
-1 ° 01 2 ° 01 2 ° 02 3 ° 01 3 ° 02 4 ° 01 4 ° 02 5 ° 01 5 ° 02 5 ° 03 6 ° 01 6 ° 02 6 ° 03 7 ° 01 7 ° 02 7 ° 03 8 ° 01 8 ° 02 8 ° 03 9 ° 01 9 ° 03 10 ° 01 10 ° 02 10 ° 03 11 ° 01 11 ° 02 11 ° 03 11 ° 04
+);
+CREATE TABLE votos (
+    id_voto INT AUTO_INCREMENT PRIMARY KEY,
+    id_candidato INT,
+    cantidad INT DEFAULT 1,
+    FOREIGN KEY (id_candidato) REFERENCES candidatos(id_candidato)
+);
+
+
+
+1°01
+2°01
+2°02
+3°01
+3°02
+4°01
+4°02
+5°01
+5°02
+5°03
+6°01
+6°02
+6°03
+7°01
+7°02
+7°03
+8°01
+8°02
+8°03
+9°01
+9°02
+9°03
+10°01
+10°02
+10°03
+11°01
+11°02
+11°03
+11°04
